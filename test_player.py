68c3f21c81dd68ed66e170a9512fb5e79f000706
sondos <sondos.fag@gamil.com>
2024-04-23 20:08:41 +0200

    wip

--- test_player.py
+++ test_player.py
@@ -7,11 +7,13 @@ class TestPlayer(unittest.TestCase):
         self.player = Player("Sondos", False)
     
     def test_get_name(self):
-            self.assertEqual(self.player.get_name(), "Sondos")
+        self.assertEqual(self.player.get_name(), "Sondos")
     
     def test_get_is_computer(self):
-            self.assertFalse(self.player.get_is_computer())
+        self.assertFalse(self.player.get_is_computer())
     
     def test_set_name(self):
-            self.player.set_name("Sara")
-            self.assertEqual(self.player.get_name(), "Sara")
+        self.player.set_name("Sara")
+        self.assertEqual(self.player.get_name(), "Sara")
+    if __name__ == '__main__':
+        unittest.main()
