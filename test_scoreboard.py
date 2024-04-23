6cda2c834d312d78de997999401b60374bf2d485
Sara <sara.alsheikh01@gmail.com>
2024-04-23 21:18:31 +0200

    added test for Scoreboard initialization

--- test_scoreboard.py
+++ test_scoreboard.py
@@ -6,3 +6,8 @@ class TestScoreboard(unittest.TestCase):
     def setUp(self): 
         self.players = ["Sara", "Sondos", "Ali"]
         self.scoreboard = Scoreboard(self.players)
+    
+    def test_initialization(self):
+        self.assertEqual(self.scoreboard.maximum_score, 100)
+        self.assertIsNone(self.scoreboard.wining_player)
+        self.assertEqual(self.scoreboard.player_scores, [["Sara", 0], ["Sondos", 0], ["Ali", 0]])
