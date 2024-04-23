46a6dbf48c6dd9d9f858b82c04100ea04ef373d1
sondos <sondos.fag@gamil.com>
2024-04-23 19:53:08 +0200

    creating constructor for the players class

--- Player.py
+++ Player.py
@@ -1,2 +1,7 @@
 class Player:
     """players class"""
+
+    def __init__(self, name, computer):
+        """"Initialize a player with a name and computer with true or false""""
+        self.name = str(name)
+        self.computer = bool(computer)
