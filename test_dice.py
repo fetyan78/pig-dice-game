919382c9075dfcb2e3820c0666cb937951b5f582
sondos <sondos.fag@gamil.com>
2024-04-23 19:38:35 +0200

    creating function to test the roll

--- test_dice.py
+++ test_dice.py
@@ -5,3 +5,15 @@ class TestDice(unittest.TestCase):
 
     def setUp(self):
         self.dice = Dice()
+
+    def test_roll(self):
+        result = self.dice.roll()
+        rolls = result['rolls']
+        total = result['total']
+        self.assertIsInstance(rolls, tuple)
+        self.assertEqual(len(rolls), 2)
+        self.assertTrue(1 <= rolls[0] <= 6)
+        self.assertTrue(1 <= rolls[1] <= 6)
+        self.assertTrue(2 <= total <= 12)
+if __name__ == '__main__':
+    unittest.main()
