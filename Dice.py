42f113387376505c4fc95dce8bf24f8ffa854cf5
sondos <sondos.fag@gamil.com>
2024-04-23 19:26:47 +0200

    Creating function for rolling the dice

--- Dice.py
+++ Dice.py
@@ -7,3 +7,10 @@ class Dice:
         """initialize the dice"""
         self.sides = [1, 2, 3, 4, 5, 6]
         random.shuffle(self.sides)
+    
+    def roll(self):
+        """roll the dice"""
+        roll1 = random.choice(self.sides)
+        roll2 = random.choice(self.sides)
+        total = roll1 + roll2
+        return {'rolls': (roll1, roll2), 'total': total}
