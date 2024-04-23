c6fbd65e16172307f6368f244f7e6adb617ba114
Sara <sara.alsheikh01@gmail.com>
2024-04-23 19:27:03 +0200

    creating update score function

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,2 +1,13 @@
 class Scoreboard: 
     def __init__(self, players_list):
+        self.maximum_score = 100 
+        self.wining_player = None 
+        self.player_scores = []
+        for player in players_list: 
+            self.player_scores.append([player,0])
+    
+    def update_score(self, name, score):
+        for i, player_score in enumerate(self.player_scores): 
+            if name == player_score [0]:
+                self.player_scores[i][1] += score 
+    
