d6ca5dc166eb626a96698408906f0addd1842416
sondos <sondos.fag@gamil.com>
2024-04-23 19:20:24 +0200

    Creating constructor for class

--- Dice.py
+++ Dice.py
@@ -2,3 +2,8 @@ import random
 
 class Dice:
     """class for a six dice"""
+
+    def __init__(self):
+        """initialize the dice"""
+        self.sides = [1, 2, 3, 4, 5, 6]
+        random.shuffle(self.sides)
