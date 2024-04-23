334f85cb81ab1605b3666262a0ddef191ce3d6ab
Sara <sara.alsheikh01@gmail.com>
2024-04-23 19:52:45 +0200

    added resert_score method

--- Scoreboard.py
+++ Scoreboard.py
@@ -15,5 +15,10 @@ class Scoreboard:
         for i, player_score in enumerate(self.player_scores):
             if current_name == player_score[0]: 
                 self.player_scores[i][0] = new_name
+    
+    def resert_score(self, name):
+        for i, player_score in enumerate(self.player_scores):
+            if name == player_score[0]:
+                self.player_scores[i][1] = 0 
                 
     
